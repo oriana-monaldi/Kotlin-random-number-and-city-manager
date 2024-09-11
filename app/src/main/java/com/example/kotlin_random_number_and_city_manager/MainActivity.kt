7b10e7c17d545b6d8764299668f601e9c04345a5
mateopolci <mateopolci@hotmail.com>
2024-09-11 12:13:20 -0300

    main menu and theme

--- app/src/main/java/com/example/kotlin_random_number_and_city_manager/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_random_number_and_city_manager/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import com.example.kotlin_random_number_and_city_manager.ui.theme.KotlinrandomnumberandcitymanagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinrandomnumberandcitymanagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Menu()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Menu() {
+    val selectedModule = remember { mutableStateOf("Ninguno") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Módulo seleccionado: ${selectedModule.value}",
+            modifier = Modifier.padding(bottom = 80.dp)
+        )
+
+        Button(
+            onClick = { selectedModule.value = "Adivinar el número" },
+            modifier = Modifier
+                .width(180.dp)
+                .height(50.dp),
+
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = White
+            )
+        ) {
+            Text(text = "Adivinar el número")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { selectedModule.value = "Ciudades del mundo" },
+            modifier = Modifier
+                .width(180.dp)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = White
+            )
+        ) {
+            Text(text = "Ciudades del mundo")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MenuPreview() {
     KotlinrandomnumberandcitymanagerTheme {
-        Greeting("Android")
+        Menu()
     }
 }
--- app/src/main/java/com/example/kotlin_random_number_and_city_manager/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kotlin_random_number_and_city_manager/ui/theme/Theme.kt
@@ -9,34 +9,28 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.Black,
+    onPrimary = Color.White,
+    secondary = Color.Gray,
+    onSecondary = Color.White,
+    tertiary = Color(0xFFBB86FC)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Color.Black,
     onPrimary = Color.White,
+    secondary = Color.Gray,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Color(0xFFBB86FC)
 )
 
 @Composable
 fun KotlinrandomnumberandcitymanagerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
